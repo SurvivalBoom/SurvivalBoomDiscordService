68d05ea731a61c1b24d242f6a26093129c94451c
Місяць <120984956+Mycetik@users.noreply.github.com>
2026-09-07 06:08:39 +0300

    ContextInteractionInfo: Додано Raw методи

--- api/src/main/java/net/survivalboom/sbds/api/commands/context/ContextInteractionInfo.java
+++ api/src/main/java/net/survivalboom/sbds/api/commands/context/ContextInteractionInfo.java
@@ -1,6 +1,8 @@
 package net.survivalboom.sbds.api.commands.context;
 
 import net.dv8tion.jda.api.events.interaction.command.GenericContextInteractionEvent;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 import net.survivalboom.sbds.api.ISBDS;
 import net.survivalboom.sbds.api.commands.Command;
 import net.survivalboom.sbds.api.interaction.InteractionExecutionInfo;
@@ -25,6 +27,62 @@ public abstract class ContextInteractionInfo<E extends GenericContextInteraction
         this.rootCommand = rootCommand;
         this.currentCommand = currentCommand;
         this.alias = alias;
+    }
+
+    // EDIT //
+
+    @Override
+    public @NotNull RestAction<?> editRaw(@NotNull String txt) {
+        if (!event.isAcknowledged()) {
+            throw new IllegalStateException("Cannot edit original message because no message was sent yet. Did you forget to call deferReply?");
+        }
+        return event.getHook().editOriginal(txt);
+    }
+
+    @Override
+    public @NotNull RestAction<?> editRaw(@NotNull MessageCreateData data) {
+        if (!event.isAcknowledged()) {
+            throw new IllegalStateException("Cannot edit original message because no message was sent yet. Did you forget to call deferReply?");
+        }
+        return event.getHook().editOriginal(net.dv8tion.jda.api.utils.messages.MessageEditData.fromCreateData(data));
+    }
+
+    // SEND ONLY //
+
+    @Override
+    public @NotNull RestAction<?> sendRaw(@NotNull String txt) {
+        if (event.isAcknowledged()) {
+            return event.getHook().sendMessage(txt).setEphemeral(currentCommand.isEphemeral());
+        }
+        return event.reply(txt).setEphemeral(currentCommand.isEphemeral());
+    }
+
+    @Override
+    public @NotNull RestAction<?> sendRaw(@NotNull MessageCreateData data) {
+        if (event.isAcknowledged()) {
+            return event.getHook().sendMessage(data).setEphemeral(currentCommand.isEphemeral());
+        }
+        return event.reply(data).setEphemeral(currentCommand.isEphemeral());
+    }
+
+    // REPLY //
+
+    @Override
+    public @NotNull RestAction<?> replyRaw(@NotNull String txt) {
+        if (event.isAcknowledged()) {
+            return editRaw(txt);
+        } else {
+            return sendRaw(txt);
+        }
+    }
+
+    @Override
+    public @NotNull RestAction<?> replyRaw(@NotNull MessageCreateData data) {
+        if (event.isAcknowledged()) {
+            return editRaw(data);
+        } else {
+            return sendRaw(data);
+        }
     }
 
     public @NotNull IContextCommandManager.IRegisteredContextCommand rootCommand() {
